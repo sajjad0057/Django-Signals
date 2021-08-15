192825b48a08ae0c5641d1efe00eab668d3d8932
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-15 18:47:56 +0600

    deactive order using pre_delete signals

--- orders/signals.py
+++ orders/signals.py
@@ -1,8 +1,8 @@
-from typing import Sequence
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed,post_save
 from django.dispatch import receiver
 
 from .models import Order
+from sales.models import Sale
 
 
 @receiver(m2m_changed, sender=Order.cars.through)
@@ -20,3 +20,13 @@ def m2m_change_cars_order(sender, instance, action, **kwargs):
         instance.total = total
         instance.total_price = total_price
         instance.save()
+
+
+
+
+@receiver(post_save,sender=Order)
+def post_save_create_or_update_sale(sender,instance,created,**kwargs):
+    obj , _ = Sale.objects.get_or_create(order=instance)
+    obj.amount = instance.total_price
+    obj.save()
+
--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Sale
 
 # Register your models here.
+
+
+class SaleAdmin(admin.ModelAdmin):
+    fileds = ['order','amount']
+    list_display = ['order','amount']
+
+admin.site.register(Sale,SaleAdmin)
