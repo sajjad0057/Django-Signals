12699450599c7a51884942e25768c4ef5fa421ac
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-15 18:26:40 +0600

    orders signals (m2m_changed)

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fields = ['cars','name','total','total_price','active']
+    list_display = ['name','total','total_price','active']
+
+admin.site.register(Order,OrderAdmin)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cars.models import Car
+
 
 # Create your models here.
+
+
+class Order(models.Model):
+    cars = models.ManyToManyField(Car)
+    name = models.CharField(max_length=256)
+    total = models.PositiveIntegerField(blank=True,null=True)
+    total_price = models.PositiveIntegerField(blank=True,null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.name)
