55c7b3a5312068d771a6ca8cb98de61d327699e9
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-15 16:53:07 +0600

    create buyer instance by creating user

--- buyers/admin.py
+++ buyers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Buyer
 
 # Register your models here.
+
+class BuyerAdmin(admin.ModelAdmin):
+    fields = ['user','from_signal']
+    list_display = ('user','from_signal')
+
+
+admin.site.register(Buyer,BuyerAdmin)
--- buyers/apps.py
+++ buyers/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BuyersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'buyers'
+
+
+    def ready(self):
+        import buyers.signals 
+         
